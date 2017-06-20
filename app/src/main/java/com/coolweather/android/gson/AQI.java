ac765ba89f310eaebb6aaeb27cded5de5365171a
zk <a1124013482@qq.com>
2017-06-20 17:31:53 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by zk on 2017/6/18.
+ */
+
+public class AQI {
+    public AQICity ctiy;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
